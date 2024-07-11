1bcc1fa9d27cfaab8d5aa52b9df4cfc427c71fb3
ALANMBO <bejaranomauricio999@gmail.com>
2024-07-11 18:18:47 -0400

    Paso 1 para la validación a la base de datos

--- src/course/entities/course.entity.ts
+++ src/course/entities/course.entity.ts
@@ -1,7 +1,11 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Course {
     @PrimaryGeneratedColumn(  )
-id: number;
+    id: number;
+    @Column({ length: 500 })
+    NombreMateria: string;
+    @Column('text')
+    Descripcion: string;
 }
